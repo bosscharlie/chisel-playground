f97878e785857ec0ed64773ecff0536f4712018d
Charlie Liu <878574650@qq.com>
2023-11-30 11:08:26 +0800

    fix server compile error & add options to elaborate stage

--- playground/src/Elaborate.scala
+++ playground/src/Elaborate.scala
@@ -3,5 +3,9 @@ import circt.stage._
 object Elaborate extends App {
   def top = new GCD()
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
-  (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
+
+  (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.SystemVerilog):+
+    FirtoolOption("-disable-all-randomization"):+
+    FirtoolOption("-strip-debug-info")
+  )
 }
